3d5d6cdda26f197c756f22c7aba51829d138f994
Md-Sifat_code <codewithsifat6@gmail.com>
2024-09-23 01:06:53 +0600

    adding one more html page for Blogs

---
+++ js/blog.js
@@ -0,0 +1,3 @@
+document.getElementById('home').addEventListener('click',function(){
+    window.location.href = "index.html";
+})
---
+++ js/index.js
@@ -0,0 +1,7 @@
+// for Blog button
+document.getElementById('Blog').addEventListener('click',function(){
+    window.location.href = "blog.html";
+})
+// for home button
+
+
